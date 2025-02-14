4800c61f716ac9de82c593eb6ed2fbb25bc4d5a3
Amanatri0 <atriaman8897@gmail.com>
2025-02-14 08:00:28 +0530

    Backend

--- src/middleware/accountOfUser.ts
+++ src/middleware/accountOfUser.ts
@@ -10,7 +10,7 @@ declare global {
 import { NextFunction, Response, Request } from "express";
 import { PrismaClient } from "@prisma/client";
 import { JWT_SECRET } from "../config";
-import jwt, { JwtPayload } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 const client = new PrismaClient();
 
@@ -72,67 +72,3 @@ async function userAccountMiddleware(
 }
 
 export { userAccountMiddleware };
-
-//------------------------------------------------------------Middleware using second approach------------------------------------------------------------------------------------------
-
-/*
-Here the second approach which will take "userMiddelware" and await it so that when the userMiddleware completes it process.
-Only then the accountMiddleware will work
-
-this approach takes one middleware as function and then extracts the token from the middleware. 
-it seems like this one is more optimial
-
-*/
-
-//-----------//
-
-/*
-
-async function userAccountMiddleware(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  try {
-    // take the user token and extract the user email from the "userMiddleware" after the userMiddleware has success then proceed
-
-    // await new Promise<void>((resolve, reject) => {
-    //   userMiddleware(req, res, (err) => {
-    //     if (err) reject(err);
-    //     else resolve();
-    //   });
-    // });
-
-
-    const user = req.user; // users email from the token has been extracted here
-
-    const usersEmail = await client.user.findFirst({
-      where: {
-        email: user,
-      },
-    });
-
-    if (!usersEmail) {
-      return next(new Error("User email is not present in the database"));
-    }
-
-    // find the userId in the Accounts table
-    const userAccountDetails = await client.account.findFirst({
-      where: {
-        userId: usersEmail.id,
-      },
-    });
-
-    // send the Account Id that is associated with the User Id
-    req.userAccountId = userAccountDetails?.id;
-    next();
-  } catch (error) {
-    res.status(403).send({
-      message:
-        "User account middleware unable to fetch the user accounts details",
-      error: (error as Error).message,
-    });
-  }
-}
-
- */
